368378ed691a6074f2eb64ea224fd2d437040a35
draschke <draschke@googlemail.com>
2021-10-09 15:25:56 +0000

    index.md

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -7,7 +7,7 @@ let email = "draschke@googlemail.com";
 
 module.exports = {
   lang: "en-US",
-  title: "VS Code Dev Container for SAP HANA App development for Node.js 16.x",
+  title: "VS Code Dev Container for SAP HANA App development",
   description:
     "VS Code Dev Container for SAP HANA App development for Node.js 16.x",
   base: "/vsc-sap-hana-mta-dev-env-node16x/",
